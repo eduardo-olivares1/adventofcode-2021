518abdff386def8c548c3d51d787abb21b432809
Eduardo Olivares <28717476+eduardo-olivares1@users.noreply.github.com>
2021-12-01 20:19:10 -0800

    update comment

--- adventofcode_2021/___main__.py
+++ adventofcode_2021/___main__.py
@@ -7,13 +7,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 filepath = os.path.join(basedir, "data", "input", "input.txt")
 input_list = sonar_sweep.file_to_list(filepath)
 
-# Part 1 solution
+# Day 1: Part 1 solution
 number_of_increases = sonar_sweep.count_increases(input_list)
 print(
     f"Number of measurements are larger than the previous measurement: {number_of_increases}"
 )
 
-# Part 2 solution
+# Day 1: Part 2 solution
 sliding_window_arr = sonar_sweep.sliding_window_sum(input_list)
 sliding_window_increases = sonar_sweep.count_increases(sliding_window_arr)
 print(
